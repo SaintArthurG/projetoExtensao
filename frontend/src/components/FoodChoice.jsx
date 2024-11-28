f219cdea8a1230d7b11d3918608b05ca15252117
SaintArthurG <cazooklol@gmail.com>
2024-11-27 21:48:11 -0300

    Revert "Mudança no app.jsx e Footer"
    This reverts commit c845d16200065ec1f7e1e466b43af4190a5f0c08.

--- frontend/src/components/FoodChoice.jsx
+++ frontend/src/components/FoodChoice.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
-import styles from './FoodChoice.module.css';
 
 const FoodChoice = ({ title, options, selected, onChange, multiSelect = false }) => {
   return (
-    <div className={styles.container}>
-      <h3 className={styles.title}>{title}</h3>
-      <ul className={styles.list}>
+    <div>
+      <h3>{title}</h3>
+      <ul>
         {options.map(option => (
-          <li key={option} className={styles.listItem}>
-            <label className={styles.label}>
+          <li key={option}>
+            <label>
               <input
-                className={styles.input}
                 type={multiSelect ? 'checkbox' : 'radio'}
                 value={option}
                 checked={multiSelect ? selected.includes(option) : selected === option}
                 onChange={() => onChange(option)}
               />
-              <span>{option}</span>
+              {option}
             </label>
           </li>
         ))}
